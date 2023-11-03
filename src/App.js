06acc75c734edaed92d8e9750de20342c484eb0e
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2023-11-03 08:58:54 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
+  console.log("halo");
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+            alt="tes"
+            className="movie-image"
+          />
+          <div className="movie-date">Release: {movie.release_date}</div>
+          <div className="movie-rate">{movie.vote_average.toFixed(1)}</div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Movie Mania</h1>
+        <input
+          type="text"
+          placeholder="Cari film kesayangan...."
+          className="movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+
+        <div className="movie-container">
+          <PopularMovieList />
+        </div>
+      </header>
+    </div>
+  );
+};
+export default App;
